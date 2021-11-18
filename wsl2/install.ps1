fb3bf724b52cb64ee4fdb552d410b5d5146107ce
Dorin Andrei Dragan <dorin.andrei.dragan@gmail.com>
2021-11-18 23:32:33 +0200

    Comment for WSL2 Linux Kernel Update

--- wsl2/install.ps1
+++ wsl2/install.ps1
@@ -8,6 +8,19 @@ Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRes
 # Enable WSL
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All -NoRestart
 
+# Update Linux Kernel to latest version
+# $wslUpdateMsi = "$PSScriptRoot/wsl_update_x64.msi"
+# downloadFile "https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi" $wslUpdateMsi
+# try {
+#   Start-Process msiexec.exe -Wait -ArgumentList "/I $wslUpdateMsi /quiet"
+# }
+# catch {
+#   Write-Error "WSL Update failed: $($_.Exception.Message)" 
+# }
+# finally {
+#   Remove-Item -Force $PSScriptRoot/wsl_update_x64.msi
+# }
+
 # Install Ubuntu distro
 run "wsl" @("--install", "-d", "Ubuntu")
 
