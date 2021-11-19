eab3081510bee059093cc289fefadcc48e8fae4e
Dorin Andrei Dragan <dorin.andrei.dragan@gmail.com>
2021-11-19 14:04:43 +0200

    Comment out the WSL2 uninstall

--- cleanup.ps1
+++ cleanup.ps1
@@ -5,7 +5,8 @@
 # development
 & $PSScriptRoot/vscode/uninstall.ps1
 & $PSScriptRoot/vs2022/uninstall.ps1
-& $PSScriptRoot/wsl2/uninstall.ps1
+# Comment out the WSL2 uninstaller for now.
+# & $PSScriptRoot/wsl2/uninstall.ps1
 
 # devops
 & $PSScriptRoot/vagrant/uninstall.ps1
