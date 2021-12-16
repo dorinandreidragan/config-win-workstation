51ed51ed2901ce107930ec2d3f034dd57f834286
dorinandreidragan <dorinandreidragan@users.noreply.github.com>
2021-12-16 13:29:34 +0200

    Add -Force to remove prompt for confirmation

--- pwsh/config.ps1
+++ pwsh/config.ps1
@@ -1,6 +1,6 @@
-Install-Module oh-my-posh -Scope CurrentUser -AcceptLicense
-Install-Module posh-git -Scope CurrentUser -AcceptLicense
-Install-Module Terminal-Icons -Scope CurrentUser -AcceptLicense
+Install-Module oh-my-posh -Scope CurrentUser -AcceptLicense -Force
+Install-Module posh-git -Scope CurrentUser -AcceptLicense -Force
+Install-Module Terminal-Icons -Scope CurrentUser -AcceptLicense -Force
 
 & $PSScriptRoot/set-profile.ps1
 & $PSScriptRoot/install-fonts.ps1
